efd89b638f2508d2925cb3bc089b47f452d6282f
Damian Isaías <L24550379@chihuahua2.tecnm.mx>
2024-11-04 16:51:21 -0700

    Add files via upload

---
+++ EVA3_METODOS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_metodos;
+
+/**
+ *
+ * @author eddyi
+ */
+public class EVA3_METODOS {
+
+    public static void main(String[] args) {
+    //USO DE LOS MÉTODOS:
+    //INVOCACIÓN O LLAMADA
+    imprimirMensaje("Hello niggers");
+        
+        
+    }
+    
+    //DECLARACIÓN
+    //header (encabezado)
+    //IMPRIMIR UN MENSAJE
+    //JAVA: Iniciar en minúsculas, verbo, siguientes palabras Inician en Mayúsculas
+    public static void imprimirMensaje(String mensaje){
+    
+        System.out.println("TU MENSAJE:  " + mensaje);
+    }
+}
